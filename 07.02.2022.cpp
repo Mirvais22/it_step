94519d8003fc752df228b5a86e44adead03953ae
Mirvais22 <94544939+Mirvais22@users.noreply.github.com>
2022-02-07 21:48:46 +0600

    Create 07.02.2022.cpp

---
+++ 07.02.2022.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	//1
+	/*int a = 0;
+	cin >> a;
+	int b = 1;
+	int c = 1;
+	int d = 1;
+
+	for (int i = 0; i < 4; i++) {
+		b = b * a;
+	}
+	cout << b << endl;
+	for (int i = 0; i < 6; i++) {
+		d = d * a;
+	}
+	cout << d << endl;
+	for (int i = 0; i < 15; i++) {
+		c = c * a;
+	}
+	cout << c << endl;*/
+
+	//2
+	/*double x, y ,z;
+	cin >> x ;
+	cin >> y;
+	cin >> z;
+
+	cout << (x/z)*(y/z) << endl;*/
+
+	//3
+	//float a, b, c;
+	//cin >> a;
+	//cin >> b;
+	//cin >> c;
+	//if (a < b && a < c) {
+	//	cout << "Da "<< endl;
+	//}
+	//else if (b > a && b > c) {
+	//	cout << "Da "<< endl;
+	//}
+	//else {
+	//	cout << "Net";
+	//}
+
+	//4
+	//int a = 3;
+	//int b = 4;
+	//int c = 7;
+	//if (a = b)
+	//	cout << "da";
+	//else if (b = c)
+	//	cout << "da";
+	//else if (c = a)
+	//	cout << "da";
+	//else
+	//	cout << "net";
+
+	//5
+	//for (int i = 0; i < 4; i++) {
+	//	cout<<"*******" << endl;
+	//}
+
+	//6
+	/*int a = 1;
+	int b = 0;
+
+	for (int i = 0; i < 37; i++) {
+		a = a + 3;
+		cout << a << " + ";
+		b = b + a;
+	}
+
+	cout << endl;
+	cout << b;*/
+
+
+	/*int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 123;
+
+	a = d / 100;
+	cout<<a<<endl;
+
+	b = (d % 100) / 10;
+	cout << b<<endl;
+
+	c = d % 10;
+	cout << c << endl;*/
+}
