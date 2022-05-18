c962c404c1388729862d7919202258183354b476
Mirvais22 <94544939+Mirvais22@users.noreply.github.com>
2022-05-18 21:40:58 +0600

    Create 18.05.2022.cpp

---
+++ 18.05.2022.cpp
@@ -0,0 +1,307 @@
+#ifndef _s_list_h
+#define _s_list_h
+
+#include <iostream>
+
+/**
+Структура как элемент списка и хранения данных.
+*/
+template<class T>
+struct node
+{
+	T data;			// Данные
+	node<T>* next;	// Адрес следующего элемента списка
+	node() : next(nullptr) {}
+};
+
+/**
+Реализация класса итератора для организации доступа
+к элементам контейнера.
+*/
+template<class T>
+class s_iterator
+{
+	node<T>* ptr;
+public:
+	s_iterator() : ptr(nullptr) {}
+	s_iterator(node<T>* ptr) : ptr(ptr) {}
+	s_iterator& operator++() { ptr = ptr->next; return *this; }
+	T& operator*() const { return ptr->data; }
+	node<T> operator->() { return ptr; }
+	bool operator!=(const s_iterator& obj) const { return ptr != obj.ptr; }
+	~s_iterator() { ptr = nullptr; }
+};
+
+/**
+Описание класса списка.
+*/
+template<class T>
+class s_list {
+	node<T>* first;									// адрес первого элемента списка
+	node<T>* last;									// адрес последнего элемента списка
+public:
+	friend class s_iterator<T>;						// включение класса итератора
+	s_list();										// конструктор по умолчанию
+	s_list(const s_list<T>& obj);					// конструктор копирования
+	s_list(s_list<T>&& obj);						// конструктор перемещения
+	s_list<T>& operator=(const s_list<T>& obj);		// оператор равно копированием
+	s_list<T>& operator=(s_list<T>&& obj);			// оператор равно перемещением
+	T& operator[](size_t pos) const;				// оператор индексирования
+
+	void add_to_head(const T& data);				// добавление элемента в начало 
+	void add_to_tail(const T& data);				// добавление элемента в конец 
+	T front() const;								// возвращает первое значение
+	T back() const;									// возвращает последнее значение
+	T delete_from_head();							// удаление первого элемента
+	T delete_from_tail();							// удаление последнего элемента
+	void delete_all();								// удаление всех элементов
+	void show() const;								// отображение всех элементов
+	size_t size() const;							// количество элементов
+
+	s_iterator<T> begin() const;					// возвращает итератор на первый элемент
+	s_iterator<T> end() const;						// возвращает итератор на элемент, следующий за последним
+
+	void insert_at(size_t pos, const T& data);		// вставка в указанную позицию
+	void delete_at(size_t pos);						// удаление по указанной позиции
+	size_t find(const T& data) const;				// поиск элемента (NULL в случае неудачи)
+	size_t find_and_replace(const T& data, const T& repl_data);	// поиск и замена всех элементов (возвращаем кол-во замен)
+	void reverse();									// переворот списка
+
+	void sort();									// сортирует элементы (вставками)
+	void splice(s_list<T>& obj);					// перемещает элементы из другого list 
+
+	~s_list();										// деструктор
+private:
+	void add_to_head(node<T>* ptr);					// добавляет ноду в голову списка
+	void add_to_tail(node<T>* ptr);					// добавляет ноду в конец списка
+};
+
+
+template<class T>
+inline void s_list<T>::insert_at(size_t pos, const T& data)
+{
+
+	size_t index{ 0 };
+	node<T>* temp{ first };
+	while (temp) {
+		if (index == pos) {
+			node<T>* newnode = new node<T>*();
+			newnode->data = data;
+			newnode->next = temp->next;
+			temp->next = newmode;
+			break;
+		}
+		index++;
+		temp = temp->next;
+	}
+
+}
+
+template<class T>
+inline void s_list<T>::delete_at(size_t pos)
+{
+	size_t index{ 0 };
+	node<T>* temp{ first };
+	while (temp) {
+		index++;
+		delete temp = temp->next;
+	}
+}
+
+template<class T>
+inline size_t s_list<T>::find(const T& data) const
+{
+	node<T>* temp{ first };
+	auto tmp = NULL;
+	while (temp)
+	{
+		if (temp->next == data)
+			tmp = temp->next;
+	}
+	return tmp;
+}
+
+template<class T>
+inline void s_list<T>::reverse()
+{
+	size_t index{ 0 };
+	this->first;
+	while (this->first) {
+		node<T>* newnode = new node<T>*();
+		newnode->data = data;
+		newnode->next = this->last->next - 1;
+		this->first->next = newnode;
+
+		index++;
+		this->first = this->last->next - 1;
+	}
+}
+
+template<class T>
+inline s_list<T>::s_list() : first(nullptr), last(nullptr){}
+
+template<class T>
+inline s_list<T>::s_list(const s_list<T>& obj)
+{
+	node<T>* temp{ obj.data };
+	while (temp)
+	{
+		add_to_tail(temp->data);
+		temp = temp->next;
+	}
+}
+
+template<class T>
+inline s_list<T>::s_list(s_list<T>&& obj)
+{
+	this->data = obj.data;
+	this->next = obj.next;
+	obj.data = nullptr;
+}
+
+template<class T>
+inline s_list<T>& s_list<T>::operator=(const s_list<T>& obj)
+{
+	this->data = obj.data;
+	if (this->data) {
+		delete this->data;
+	}
+	this->next = new int[obj.data];
+	for (auto i = 0; i < obj.next; i++) {
+		this->next = obj.next;
+	}
+	return *this;
+	// TODO: вставьте здесь оператор return
+}
+
+template<class T>
+inline s_list<T>& s_list<T>::operator=(s_list<T>&& obj)
+{
+	this->first = obj.first;
+	this->last = obj.last;
+	obj.first(nullptr);
+	obj.last(nullptr);
+	// TODO: вставьте здесь оператор return
+}
+
+
+template<class T>
+inline void s_list<T>::show() const
+{
+	node<T>* temp{ first };
+	while (temp)
+	{
+		if (temp->next == NULL)
+		{
+			break;
+		}
+		cout << temp->next << " ";
+	}
+}
+
+template<class T>
+inline size_t s_list<T>::size() const
+{
+	size_t index{ 0 };
+	node<T>* temp{ first };
+	while (temp) {
+		index++;
+		temp = temp->next;
+	}
+	return index + 1;
+}
+
+template<class T>
+inline s_iterator<T> s_list<T>::begin() const
+{
+	return first;
+}
+
+template<class T>
+inline s_iterator<T> s_list<T>::end() const
+{
+	return last->next;
+}
+
+template<class T>
+inline void s_list<T>::sort()
+{
+	node<T>* temp{ first };
+	int buff{ 0 };
+	for (auto i = begin(); i < end(); i++)
+	{
+		buff = temp[i];
+		for (auto j = i - 1; j >= 0 && temp[j] > buff; j++)
+		{
+			temp[j + 1] = temp[i];
+			temp[j + 1] = buff;
+		}
+	}
+}
+
+template<class T>
+inline s_list<T>::~s_list()
+{
+	delete first;
+	delete last;
+}
+
+template<class T>
+inline void s_list<T>::add_to_head(const T& data)
+{
+	size_t index{ 0 };
+	this->first;
+	while (this->first) {
+		node<T>* newnode = new node<T>*();
+		newnode->data = data;
+		newnode->next = this->first->next;
+		this->first->next = newnode;
+
+		index++;
+		this->first = this->first->next;
+	}
+}
+
+template<class T>
+inline void s_list<T>::add_to_tail(const T& data)
+{
+	node<T>* newnode = new node<T>();
+	newnode->data = data;
+	if (first == last)
+		first = last = newnode;
+	else
+	{
+		last->next = newnode;
+		last = newnode;
+	}
+}
+
+template<class T>
+inline T s_list<T>::front() const
+{
+	return first();
+}
+
+template<class T>
+inline T s_list<T>::back() const
+{
+	return last();
+}
+template<class T>
+inline T s_list<T>::delete_from_head()
+{
+	first = nullptr;
+}
+template<class T>
+inline T s_list<T>::delete_from_tail()
+{
+	last = nullptr;
+	
+}
+template<class T>
+inline void s_list<T>::delete_all()
+{
+	delete first;
+	delete last;
+}
+#endif
